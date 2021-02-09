59a09feba236e75b019ae4839042e4cfccdf6aa6
sousajo <josesousa@rentalcars.com>
2021-02-09 18:25:31 +0000

    WIP #4 Refactor Webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,15 @@
 const path = require("path");
 
+const postCssPlugins = {
+  plugins: [
+    require("postcss-simple-vars"),
+    require("postcss-nested"),
+    require("autoprefixer"),
+  ],
+};
+
+
+
 module.exports = {
   entry: "./app/assets/scripts/App.js",
 
@@ -10,4 +20,38 @@ module.exports = {
 
   mode: "development",
   watch: true,
+
 };
+
+// module.exports = {
+//   entry: "./app/assets/scripts/App.js",
+
+//   output: {
+//     filename: "bundled.js",
+//     path: path.resolve(__dirname, "app"),
+//   },
+
+//   mode: "development",
+//   watch: true,
+
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/i,
+//         exclude: /node_modules/,
+//         use: [
+//           "style-loader",
+//           "css-loader",
+//           {
+//             loader: "postcss-loader",
+//             options: {
+//               postcssOptions: {
+//                 postCssPlugins,
+//               },
+//             },
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// };
